b9d3da73839a6029a2ed9b0e930d578439711ebd
Chris Hodapp <hodapp@haystack>
2016-11-02 14:55:07 -0400

    Zeppelin & script: Added note on packages; fixed small pprint bug

--- ec2/zeppelin_printREPL.sh
+++ ec2/zeppelin_printREPL.sh
@@ -2,7 +2,7 @@
 
 cd /
 
-cat <<EOF | patch -p0 -b -t
+cat <<EOF | sudo /usr/bin/patch -p0 -b -t
 *** /etc/zeppelin/conf/interpreter.json_bak	2016-11-02 14:44:45.396942971 +0000
 --- /etc/zeppelin/conf/interpreter.json	2016-11-02 14:49:17.997687967 +0000
 ***************
@@ -15,5 +15,26 @@ cat <<EOF | patch -p0 -b -t
           "spark.yarn.jar": "",
           "master": "yarn-client",
           "zeppelin.spark.maxResult": "1000",
-
+***************
+*** 30,36 ****
+            "name": "sql"
+          }
+        ],
+!       "dependencies": [],
+        "option": {
+          "remote": true,
+          "perNoteSession": false,
+--- 31,42 ----
+            "name": "sql"
+          }
+        ],
+!       "dependencies": [
+!         {
+!           "groupArtifactVersion": "com.cloudera.sparkts:sparkts:0.4.0",
+!           "local": false
+!         }
+!       ],
+        "option": {
+          "remote": true,
+          "perNoteSession": false,
 EOF
