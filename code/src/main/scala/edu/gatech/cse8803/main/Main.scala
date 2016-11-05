ec7855f158c405bc48950ebd920f34f489052863
Chris Hodapp <hodapp@haystack>
2016-11-05 09:59:36 -0400

    More exploratory code (filtering labevents)

--- code/src/main/scala/edu/gatech/cse8803/main/Main.scala
+++ code/src/main/scala/edu/gatech/cse8803/main/Main.scala
@@ -1,5 +1,8 @@
 package edu.gatech.cse8803.main
 
+import edu.gatech.cse8803.util.Utils
+import edu.gatech.cse8803.util.Schemas
+
 // import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
@@ -7,15 +10,12 @@ import org.apache.spark.storage.StorageLevel
 import java.sql.Timestamp
 // import com.cloudera.sparkts._
 
-import edu.gatech.cse8803.util.Utils
-import edu.gatech.cse8803.util.Schemas
-
 case class PatientEventSeries(
   subject_id: Int,
   adm_id: Int,
   item_id: Int,
   unit: String,
-  series: Seq[(Timestamp, String)]
+  series: Seq[(java.sql.Timestamp, String)]
 )
 
 case class LabItem(
@@ -26,19 +26,20 @@ case class LabItem(
   loincCode: String
 )
 
-// This is used only for lab_ts_wtf, the test case from hell:
-case class WTF(a1 : Int, a2 : Int, a3 : Int, a4 : String)
+// This is for lab_ts, which inexplicably has a NullPointerException
+// if I instead use a 4-tuple of identical fields.
+case class LabWrapper(a1 : Int, a2 : Int, a3 : Int, a4 : String)
 
 object Main {
   def main(args: Array[String]) {
+    val spark = Utils.createContext
+    import spark.implicits._
+
     import org.apache.log4j.Logger
     import org.apache.log4j.Level
 
     Logger.getLogger("org").setLevel(Level.WARN)
     Logger.getLogger("akka").setLevel(Level.WARN)
-
-    val spark = Utils.createContext
-    import spark.implicits._
 
     // These are small enough to cache:
     val d_icd_diagnoses = Utils.csv_from_s3(
@@ -70,18 +71,30 @@ object Main {
     val diagnoses_icd = Utils.csv_from_s3(
       spark, "DIAGNOSES_ICD", Some(Schemas.diagnoses))
 
-    // Below is causing NullPointerException for some reason.  It
-    // seems to happen even if I take just the groupByKey output.  It
-    // goes away if I group on only two elements.
-    //
-    // If I don't solve this bullshit soon, I should just use the
-    // slower RDD-based method.
-    //
-    // Related to this maybe?
-    // https://issues.apache.org/jira/browse/SPARK-12063
-    val lab_ts_wtf = labevents.
+    // What is the minimum length (number of samples/events) in a
+    // time-series that we'll consider for a given admission & item?
+    val lab_min_series = 100
+    val lab_min_patients = 30
+
+    val lab_good_items = labevents.
+      groupBy("HADM_ID", "ITEMID").
+      // How long is this time-series (for the given admission & lab
+      // item)?
+      count.
+      filter($"count" >= lab_min_series).
+      groupBy("ITEMID").
+      // How many times does this item occur, given that we're
+      // concerned only with length >= lab_min_series?
+      count.
+      filter($"count" >= lab_min_patients)
+    lab_good_items.persist(StorageLevel.MEMORY_AND_DISK)
+
+    // Get lab time-series for each: subject, admission, item
+    // (i.e. which lab test), and unit (which should be identical
+    // per-item).
+    val lab_ts = labevents.
       groupByKey { r: Row =>
-        WTF(r.getAs[Int]("SUBJECT_ID"),
+        LabWrapper(r.getAs[Int]("SUBJECT_ID"),
           r.getAs[Int]("HADM_ID"),
           r.getAs[Int]("ITEMID"),
           r.getAs[String]("VALUEUOM"))
@@ -91,14 +104,23 @@ object Main {
             r.getAs[String]("VALUE"))
         }.toSeq
         group match {
-          case WTF(subj,hadm,item,uom) =>
+          case LabWrapper(subj,hadm,item,uom) =>
             PatientEventSeries(subj, hadm, item, uom, series)
         }
       }
-    lab_ts_wtf.persist(StorageLevel.MEMORY_AND_DISK)
+    lab_ts.persist(StorageLevel.MEMORY_AND_DISK)
+    // Above is causing NullPointerException for some reason if I
+    // modify the LabWrapper to instead just be a tuple (and likewise
+    // change the pattern-match to be over a tuple).  It seems to
+    // happen even if I take just the groupByKey output, and if I use
+    // 3 elements in the tuple rather than 4.  It goes away if I use
+    // only 2 elements.
+    //
+    // I also observed an issue that looked something like:
+    // https://issues.apache.org/jira/browse/SPARK-12063
 
-    // This is slower, but lacks that NullPointerException:
-    val lab_ts = labevents.
+    // This is (probably) slower, but lacks that NullPointerException:
+    val lab_ts_rdd = labevents.
       rdd.map { row =>
         ((row.getAs[Int]("SUBJECT_ID"),
           row.getAs[Int]("HADM_ID"),
@@ -115,7 +137,7 @@ object Main {
           val series = l.map { case (_,t,v) => (t,v) }.toList
           PatientEventSeries(subj, adm, item, unit, series)
       }
-    lab_ts.persist(StorageLevel.MEMORY_AND_DISK)
+    lab_ts_rdd.persist(StorageLevel.MEMORY_AND_DISK)
 
     /*
     val groups_with_type = labs_grouped.
@@ -146,8 +168,8 @@ object Main {
     val icd9counts_adm = diagnoses_icd.
       groupBy("SUBJECT_ID", "ICD9_CODE", "HADM_ID").
       count.
-      sort(desc("count")).
-      join(d_icd_diagnoses, "ICD9_CODE")
+      join(d_icd_diagnoses, "ICD9_CODE").
+      sort(desc("count"))
     
     println("Hello, World")
   }
