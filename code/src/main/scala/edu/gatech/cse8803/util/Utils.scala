4eac572ff15af37cfab49b13347195fcb7052bbc
Chris Hodapp <Hodapp87@gmail.com>
2016-11-14 18:04:07 -0500

    Fixed column-major issue it rationalQuadraticCovar
    I knew I needed column-major, and thought I was creating data in that
    form, but was thinking of something incorrectly.

--- code/src/main/scala/edu/gatech/cse8803/util/Utils.scala
+++ code/src/main/scala/edu/gatech/cse8803/util/Utils.scala
@@ -1,9 +1,14 @@
 package edu.gatech.cse8803.util
 
+import breeze.linalg.{DenseVector => BDV, DenseMatrix => BDM, sum}
+import breeze.linalg._
+import breeze.numerics._
+
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 
-object Utils {
+case object Utils {
   /**
    * Load the given CSV file, returning the dataframe for that CSV.
    * In the process, this prints out the schema inferred (for the sake
@@ -54,6 +59,38 @@ object Utils {
       Math.pow(y - x, 1/a) + b
     }
     diffs.foldLeft(Seq(0.0))((l,d) => (d + l(0)) +: l).reverse
+  }
+
+  def rationalQuadraticCovar(x1 : Iterable[Double], x2 : Iterable[Double],
+    sigma2 : Double, alpha : Double, tau : Double) : BDM[Double] =
+  {
+    /**
+      * Compute the covariance matrix using the rational quadratic
+      * function given in "Computational Phenotype Discovery" by
+      * Lasko, Denny, & Levy (2013), equation 3.
+      */
+
+    // Breeze constructs matrices column-major, thus, we want our
+    // *inner* iteration to be down a column and our outer iteration
+    // to be across the rows.  Since each element of x1 corresponds
+    // with a row, each traversal of x1 then corresponds to a column,
+    // hence, x1 is the inner iteration:
+    val data : Array[Double] = x2.flatMap {
+      t2 => x1.map { t1 => t1 - t2 }
+    }.toArray
+    val mtx = new BDM(x1.size, x2.size, data)
+    // 'mtx' now has every t1-t2
+
+    // Note parenthesis around quotient.  Precedence is a little
+    // borked, so the addition must occur outside.
+    sigma2 :* pow(((mtx :* mtx) :/ (2*alpha*tau*tau)) + 1.0, -alpha)
+  }
+
+  def sumLogLikelihood(ts : RDD[Schemas.PatientEventSeries]) = {
+    ts.map { p: Schemas.PatientEventSeries =>
+      val (x, y) = p.warpedSeries.unzip
+
+    }
   }
 
   /** Pretty-print a dataframe for Zeppelin.  Either supply
