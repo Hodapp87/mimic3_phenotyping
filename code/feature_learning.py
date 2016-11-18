2ceafd62b7827c4ad4a0d2c895af743e9da966de
Chris Hodapp <Hodapp87@gmail.com>
2016-11-17 19:11:53 -0500

    Added (running but mostly useless) Python code Keras autoencoder

--- code/feature_learning.py
+++ code/feature_learning.py
@@ -6,10 +6,32 @@ import matplotlib.pyplot as plt
 import numpy
 import math
 
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Lambda
 from keras.models import Model
+from keras.regularizers import activity_l1, l2, activity_l2
+from keras import backend as K
+from keras import objectives
+
+#######################################################################
+# Loading data
+#######################################################################
 
 input_path = "../data-temp/labs_cohort_predict_518_584.csv"
+
+# Since Spark saves the CSV data by partition, we must still find the
+# single file in that path (we coalesced to one partition, but it's
+# still not a known filename):
+files = [d for d in os.listdir(input_path) if d.endswith(".csv")]
+if len(files) != 1:
+    raise Exception("Can't find single CSV file in %s" % (input_path,))
+
+fname = os.path.join(input_path, files[0])
+print("Training: Loading from %s..." % (fname,))
+df = pandas.read_csv(fname)
+gr = list(df.groupby((df["HADM_ID"], df["ITEMID"], df["VALUEUOM"])))
+print("Training: Got %d points (%d admissions)." % (len(df), len(gr)))
+
+# 'gr' then is a list of: ((HADM_ID, ITEMID, VALUEUOM), time-series dataframe)
 
 #######################################################################
 # Loading data
@@ -30,6 +52,7 @@ print("Got %d points (%d admissions)." % (len(df), len(gr)))
 
 # 'gr' then is a list of: ((HADM_ID, ITEMID, VALUEUOM), time-series dataframe)
 
+
 #######################################################################
 # Gathering patches
 #######################################################################
@@ -40,25 +63,57 @@ print("Got %d points (%d admissions)." % (len(df), len(gr)))
 # supply a patch.
 padding = 7.5
 interval = 0.5
-minPatch = int(math.ceil(2 * padding / interval))
+patch_length = int(math.ceil(2 * padding / interval))
 
 # So, assign a weight to each time-series based on how many patches
 # are in it (in effect, make each patch equally likely):
-numPatches = numpy.array([len(ts[1])-minPatch for ts in gr])
-weights = numPatches / numPatches.sum()
+num_patches = numpy.array([len(ts[1])-patch_length for ts in gr])
+weights = num_patches / num_patches.sum()
 
-# And then select 'patchCount' indices, each one for a particular
+# And then select 'patch_count' indices, each one for a particular
 # time-series:
-patchCount = len(gr) * 3
-randomIdx = numpy.random.choice(len(gr), patchCount, p = weights)
+patch_count = len(gr) * 3
+random_idx = numpy.random.choice(len(gr), patch_count, p = weights)
 
-# Make an array to hold all this (final dimension is for mean &
-# variance):
-xTrain = numpy.zeros((patchCount, minPatch, 2))
+# Make an array to hold all this:
+x_train = numpy.zeros((patch_count, patch_length * 2))
 # Fill with (uniformly) random patches from the selected time-series:
-for (i,tsIdx) in enumerate(randomIdx):
-    ts = gr[tsIdx][1]
-    startIdx = numpy.random.randint(numPatches[tsIdx])
-    endIdx = startIdx + minPatch
-    xTrain[i,:, 0] = ts["MEAN"].iloc[startIdx:endIdx]
-    xTrain[i,:, 1] = ts["VARIANCE"].iloc[startIdx:endIdx]
+for (i,ts_idx) in enumerate(random_idx):
+    ts = gr[ts_idx][1]
+    start_idx = numpy.random.randint(num_patches[ts_idx])
+    end_idx = start_idx + patch_length
+    # First half is mean:
+    x_train[i, :patch_length] = ts["MEAN"].iloc[start_idx:end_idx]
+    # Second half is variance:
+    x_train[i, patch_length:] = ts["VARIANCE"].iloc[start_idx:end_idx]
+
+#######################################################################
+# Autoencoder
+#######################################################################
+
+# Size of hidden layers:
+hidden1 = 100
+hidden2 = 100
+
+input_ts = Input(shape=(patch_length * 2,))
+encoded1 = Dense(hidden1,
+                 activation='sigmoid',
+                 activity_regularizer=activity_l1(0.1),
+                 W_regularizer=l2(0.01))(input_ts)
+encoded2 = Dense(hidden2,
+                 activation='sigmoid',
+                 activity_regularizer=activity_l1(0.1),
+                 W_regularizer=l2(0.01))(encoded1)
+
+decoded1 = Dense(hidden1, activation='linear')(encoded2)
+decoded2 = Dense(patch_length * 2, activation='linear')(decoded1)
+
+autoencoder = Model(input=input_ts, output=decoded2)
+autoencoder.compile(optimizer='adadelta', loss='mse')
+
+autoencoder.fit(x_train, x_train,
+                nb_epoch=50,
+                batch_size=256,
+                shuffle=True,
+                #validation_data=(x_test, x_test)
+                )
